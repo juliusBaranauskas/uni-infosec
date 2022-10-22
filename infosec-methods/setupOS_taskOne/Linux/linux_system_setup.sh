7ec375601542ea9fa880cba7574144003366b038
Julius Baranauskas <22618356+juliusBaranauskas@users.noreply.github.com>
2022-10-22 23:14:44 +0300

    add initial setup files and folder structure

---
+++ infosec-methods/setupOS_taskOne/Linux/linux_system_setup.sh
@@ -0,0 +1,138 @@
+# 1. Sistemų administratorius (God), [sysadmin]
+# 2. Vadovas (Boss), [director]
+# 3. Administracija (Fin1 ir Fin2), [administration]
+# 4. Vadybininkai (Man1, Man2, Man3, Man4), [manager]
+# 5. Nežiniukas (Supreme). [peasant]
+
+
+# cleanup
+userdel God
+userdel Boss
+userdel Fin1
+userdel Fin2
+userdel Man1
+userdel Man2
+userdel Supreme
+
+groupdel sysadmin
+groupdel director
+groupdel administration
+groupdel manager
+groupdel peasant
+groupdel employee
+
+rm -rf /bendrove
+
+# Begin setup
+groupadd sysadmin
+groupadd director
+groupadd administration
+groupadd manager        
+groupadd peasant
+groupadd employee
+
+# Create users and remove passwords for them
+useradd -d /bendrove -g sysadmin God
+passwd -d God
+
+useradd -d /bendrove/boss -g director -G employee Boss
+passwd -d Boss
+
+useradd -d /bendrove/administracija/fin1/ -g administration -G employee Fin1
+passwd -d Fin1
+
+useradd -d /bendrove/administracija/fin2/ -g administration -G employee Fin2
+passwd -d Fin2
+
+useradd -d /bendrove/vadovai/man1 -g manager -G employee Man1
+passwd -d Man1
+
+useradd -d /bendrove/vadovai/man1 -g manager -G employee Man2
+passwd -d Man2
+
+useradd -d /bendrove -g peasant -G employee Supreme
+passwd -d Supreme
+
+# Kuriam direktorijas
+mkdir -p /bendrove
+setfacl -m user::---,group::---,other:---,group:employee:r-x,default:user::rwx,default:g::r-x /bendrove
+
+setfacl -m group:director:rwx,group:sysadmin:rwx,default:group:sysadmin:rwx,default:group:director:rwx /bendrove
+
+mkdir -p /bendrove/boss
+setfacl -m user::---,group::---,other:---,group:director:rwx,default:user::rwx,default:g::rwx /bendrove/boss
+chown Boss:director /bendrove/boss
+
+
+# Administracijos kampelis
+mkdir -p /bendrove/administracija
+setfacl -m user::---,group::---,other::---,group:administration:r-x,default:group:administration:r-x /bendrove/administracija
+chown root:administration /bendrove/administracija
+
+mkdir -p /bendrove/administracija/fin1
+setfacl -m u::rwx,default:user::rwx /bendrove/administracija/fin1
+chown Fin1:administration /bendrove/administracija/fin1
+
+# add higher-in-hierarchy groups permissions
+# setfacl -m group:director:rwx,group:sysadmin:rwx,default:group:sysadmin:rwx,default:group:director:rwx /bendrove/administracija/fin1
+# chmod +t /bendrove/administracija/fin1
+
+# mkdir -p /bendrove/administracija/fin2
+# setfacl 
+
+
+# Vadybininku kampelis
+mkdir -p /bendrove/vadovai
+setfacl -m user::---,group::---,other::---,group:manager:r-x,default:group:manager:r-x /bendrove/vadovai
+chown root:manager /bendrove/vadovai
+
+mkdir -p /bendrove/vadovai/man1
+setfacl -m u::rwx,default:user::rwx /bendrove/vadovai/man1
+chown Man1:manager /bendrove/vadovai/man1
+
+
+# mkdir -p bendrove/vadovai/man2
+# setfacl 
+
+
+# visi daro ka nori
+mkdir -p /bendrove/chaoso_kambarelis
+setfacl -m g:employee:rwx,other:---,default:g:employee:rwx,default:u::rwx /bendrove/chaoso_kambarelis
+
+
+# prieinamas tik tam tikriems vartotojams
+mkdir -p /bendrove/meme_club
+setfacl -m other:---,user:Fin1:rwx,user:Man1:rwx /bendrove/meme_club
+chmod +t /bendrove/meme_club
+
+
+# Task #2
+setfacl -m user:Fin2:rwx /bendrove/boss
+
+# Task #3
+sudo cp /etc/pam.d/common-password /etc/pam.d/common-password.backup
+sudo cp common-password.upgrade /etc/pam.d/common-password
+
+sudo cp /etc/login.defs /etc/login.defs.backup
+sudo cp login.defs.better /etc/login.defs
+
+
+# Task #4
+setfacl -m group:administration:r-- /usr/bin/zip
+
+setfacl -m group:employee:r-- /usr/bin/gnome-terminal
+setfacl -m group:employee:r-- /usr/bin/gnome-terminal.real
+setfacl -m group:employee:r-- /usr/bin/gnome-terminal.wrapper
+
+setfacl -m group:employee:r-- /usr/bin/ip
+
+
+# Task #6
+ownerName="Fin1"
+filename="transferOwnership.test"
+newOwner="Fin2"
+
+su - $ownerName -c "touch $filename"
+homedir=$( getent passwd "$ownerName" | cut -d: -f6 )
+chown $newOwner "$homedir$filename"
+
---
+++ infosec-methods/setupOS_taskOne/Windows/winSetup.ps1
@@ -0,0 +1,320 @@
+using namespace System.Security.AccessControl;
+
+
+# cleanup
+Remove-LocalUser -Name SystemAdmin
+Remove-LocalUser -Name BossPerson
+Remove-LocalUser -Name Fin1
+Remove-LocalUser -Name Fin2
+Remove-LocalUser -Name Man1
+Remove-LocalUser -Name Man2
+Remove-LocalUser -Name Supreme
+
+
+Remove-LocalGroup -Name "sysadmin"
+Remove-LocalGroup -Name "boss"
+Remove-LocalGroup -Name "administration"
+Remove-LocalGroup -Name "manager"
+Remove-LocalGroup -Name "employee"
+
+Remove-Item -Recurse -Path "C:\bendrove"
+
+function defaultACL {
+    param([string]$path)
+    $resAcl = Get-Acl -Path $path
+
+    $sysEntry = fullControlACL -user "NT AUTHORITY\SYSTEM"
+    $adminsEntry = fullControlACL -user "BUILTIN\Administrators"
+    $ownerEntry = fullControlACL -user "CREATOR OWNER"
+
+    $resAcl.AddAccessRule($sysEntry)
+    $resAcl.AddAccessRule($adminsEntry)
+    $resAcl.AddAccessRule($ownerEntry)
+
+    return $resAcl
+}
+
+function addUser {
+    param (
+        [string]$username,
+        [string[]]$groups
+    )
+
+    $user = New-LocalUser -Name $username -NoPassword 
+    Add-LocalGroupMember -Group $(Get-LocalGroup -Name "Users") -Member $user | Out-Null 
+
+    foreach($group in $groups) 
+    {
+        $group = Get-LocalGroup -Name $group
+        Add-LocalGroupMember -Group $group -Member $user | Out-Null
+    }
+}
+
+function fullControlACL {
+    param([string]$user)
+    
+    $rule = New-Object FileSystemAccessRule(
+            $user,
+            [FileSystemRights]::FullControl,
+            ([InheritanceFlags]::ContainerInherit + [InheritanceFlags]::ObjectInherit),
+            [PropagationFlags]::None,
+            [AccessControlType]::Allow)
+    return $rule
+}
+
+function rxACL {
+    param([string]$user)
+    
+    $rule = New-Object FileSystemAccessRule(
+            $user,
+            [FileSystemRights]::ReadAndExecute,
+            ([InheritanceFlags]::ContainerInherit + [InheritanceFlags]::ObjectInherit),
+            [PropagationFlags]::None,
+            [AccessControlType]::Allow)
+    return $rule
+}
+
+function rwxACL {
+    param([string]$user)
+    
+    $rule = New-Object FileSystemAccessRule(
+            $user,
+            ([FileSystemRights]::ReadAndExecute + [FileSystemRights]::Write),
+            ([InheritanceFlags]::ContainerInherit + [InheritanceFlags]::ObjectInherit),
+            [PropagationFlags]::None,
+            [AccessControlType]::Allow)
+    return $rule
+}
+
+function clearInheritance {
+    param([string]$path)
+    
+    $currACL = Get-Acl -Path $path
+    $currACL.SetAccessRuleProtection($true, $false)
+    Set-Acl -Path $path -AclObject $currACL
+}
+
+New-LocalGroup -Name "sysadmin"
+New-LocalGroup -Name "boss"
+New-LocalGroup -Name "administration"
+New-LocalGroup -Name "manager"
+New-LocalGroup -Name "employee"
+
+addUser -username "SystemAdmin" -groups "employee", "sysadmin", "Administrators"
+addUser -username "BossPerson" -groups "employee", "boss"
+addUser -username "Fin1" -groups "employee", "administration"
+addUser -username "Fin2" -groups "employee", "administration"
+addUser -username "Man1" -groups "employee", "manager"
+addUser -username "Man2" -groups "employee", "manager"
+addUser -username "Supreme" -groups "employee"
+
+
+#region bendrove
+$bendrovePath = "C:\bendrove"
+New-Item -Path $bendrovePath -ItemType "Directory" 
+
+# Clear inheritance
+clearInheritance -path $bendrovePath
+
+$aclObject = defaultACL -Path $bendrovePath
+
+$sysadminEntry = fullControlACL -user "sysadmin"
+$bossEntry = fullControlACL -user "boss"
+$employeeEntry = rxACL -user "employee"
+
+$aclObject.AddAccessRule($sysadminEntry)
+$aclObject.AddAccessRule($bossEntry)
+$aclObject.AddAccessRule($employeeEntry)
+Set-Acl -Path $bendrovePath -AclObject $aclObject
+#endregion
+
+
+#region BOSS
+$bossPath = "C:\bendrove\boss"
+New-Item -Path $bossPath -ItemType "Directory"
+
+# Clear inheritance
+clearInheritance -path $bossPath
+
+$aclObject = defaultACL -Path $bossPath
+
+$sysadminEntry = fullControlACL -user "sysadmin"
+$bossEntry = fullControlACL -user "boss"
+$administrationMemberEntry = rwxACL -user "Fin1"
+
+$aclObject.AddAccessRule($sysadminEntry)
+$aclObject.AddAccessRule($bossEntry)
+$aclObject.AddAccessRule($administrationMemberEntry)
+Set-Acl -Path $bossPath -AclObject $aclObject
+#endregion
+
+
+#region Administration
+$administrationPath = "C:\bendrove\administracija"
+New-Item -Path $administrationPath -ItemType "Directory"
+
+# Clear inheritance
+clearInheritance -path $administrationPath
+
+$aclObject = defaultACL -Path $administrationPath
+
+$sysadminEntry = fullControlACL -user "sysadmin"
+$bossEntry = fullControlACL -user "boss"
+$administrationEntry = rxACL -user "administration"
+
+$aclObject.AddAccessRule($sysadminEntry)
+$aclObject.AddAccessRule($bossEntry)
+$aclObject.AddAccessRule($administrationEntry)
+Set-Acl -Path $administrationPath -AclObject $aclObject
+#endregion
+
+
+#region Fin1
+$admin1Path = "C:\bendrove\administracija\fin1"
+New-Item -Path $admin1Path -ItemType "Directory"
+
+# Clear inheritance
+# clearInheritance -path $admin1Path
+
+$aclObject = defaultACL -Path $admin1Path
+
+$sysadminEntry = fullControlACL -user "sysadmin"
+$bossEntry = fullControlACL -user "boss"
+$admin1Entry = rwxACL -user "Fin1"
+
+# $aclObject.AddAccessRule($sysadminEntry)
+# $aclObject.AddAccessRule($bossEntry)
+$aclObject.AddAccessRule($admin1Entry)
+Set-Acl -Path $admin1Path -AclObject $aclObject
+#endregion
+
+
+#region Fin2
+$admin2Path = "C:\bendrove\administracija\fin2"
+New-Item -Path $admin2Path -ItemType "Directory"
+
+# Clear inheritance
+# clearInheritance -path $admin2Path
+
+$aclObject = defaultACL -Path $admin2Path
+
+$sysadminEntry = fullControlACL -user "sysadmin"
+$bossEntry = fullControlACL -user "boss"
+$admin2Entry = rwxACL -user "Fin2"
+
+# $aclObject.AddAccessRule($sysadminEntry)
+# $aclObject.AddAccessRule($bossEntry)
+$aclObject.AddAccessRule($admin2Entry)
+Set-Acl -Path $admin2Path -AclObject $aclObject
+#endregion
+
+
+#region Managers
+$managerPath = "C:\bendrove\vadovai"
+New-Item -Path $managerPath -ItemType "Directory"
+
+# Clear inheritance
+clearInheritance -path $managerPath
+
+$aclObject = defaultACL -Path $managerPath
+
+$sysadminEntry = fullControlACL -user "sysadmin"
+$bossEntry = fullControlACL -user "boss"
+$managerEntry = rxACL -user "manager"
+
+$aclObject.AddAccessRule($sysadminEntry)
+$aclObject.AddAccessRule($bossEntry)
+$aclObject.AddAccessRule($managerEntry)
+Set-Acl -Path $managerPath -AclObject $aclObject
+#endregion
+
+
+#region Man1
+$manager1Path = "C:\bendrove\vadovai\man1"
+New-Item -Path $manager1Path -ItemType "Directory"
+
+# Clear inheritance
+# clearInheritance -path $manager1Path
+
+$aclObject = defaultACL -Path $manager1Path
+
+$sysadminEntry = fullControlACL -user "sysadmin"
+$bossEntry = fullControlACL -user "boss"
+$manager1Entry = rwxACL -user "Man1"
+
+
+# $aclObject.AddAccessRule($sysadminEntry)
+# $aclObject.AddAccessRule($bossEntry)
+$aclObject.AddAccessRule($manager1Entry)
+# $aclObject.AddAccessRule($managerEntry)
+Set-Acl -Path $manager1Path -AclObject $aclObject
+#endregion
+
+
+#region Man2
+$manager2Path = "C:\bendrove\vadovai\man2"
+New-Item -Path $manager2Path -ItemType "Directory"
+
+# Clear inheritance
+# clearInheritance -path $manager2Path
+
+$aclObject = defaultACL -Path $manager2Path
+
+$sysadminEntry = fullControlACL -user "sysadmin"
+$bossEntry = fullControlACL -user "boss"
+$manager2Entry = rwxACL -user "Man2"
+
+
+# $aclObject.AddAccessRule($sysadminEntry)
+# $aclObject.AddAccessRule($bossEntry)
+$aclObject.AddAccessRule($manager2Entry)
+# $aclObject.AddAccessRule($managerEntry)
+Set-Acl -Path $manager2Path -AclObject $aclObject
+#endregion
+
+
+#region Chaoso_kambarys
+$chaosasPath = "C:\bendrove\chaoso_kambarelis"
+New-Item -Path $chaosasPath -ItemType "Directory"
+
+# Clear inheritance
+# clearInheritance -path $chaosasPath
+
+# $sysadminEntry = fullControlACL -user "sysadmin"
+# $bossEntry = fullControlACL -user "boss"
+$employeeEntry = fullControlACL -user "employee"
+
+# $aclObject.AddAccessRule($sysadminEntry)
+# $aclObject.AddAccessRule($bossEntry)
+$aclObject = defaultACL -Path $chaosasPath
+$aclObject.AddAccessRule($employeeEntry)
+Set-Acl -Path $chaosasPath -AclObject $aclObject
+#endregion
+
+
+#region meme_club
+$memeClubPath = "C:\bendrove\meme_club"
+New-Item -Path $memeClubPath -ItemType "Directory"
+
+# Clear inheritance
+clearInheritance -path $memeClubPath
+
+$aclObject = defaultACL -Path $memeClubPath
+
+$sysadminEntry = fullControlACL -user "sysadmin"
+$bossEntry = fullControlACL -user "boss"
+$manager2Entry = rwxACL -user "Man2"
+$fin1Entry = rwxACL -user "Fin1"
+
+
+$aclObject.AddAccessRule($sysadminEntry)
+$aclObject.AddAccessRule($bossEntry)
+$aclObject.AddAccessRule($manager2Entry)
+$aclObject.AddAccessRule($fin1Entry)
+Set-Acl -Path $memeClubPath -AclObject $aclObject
+#endregion
+
+# Task#3
+net accounts /maxpwage:210
+net accounts /minpwlen:9
+
